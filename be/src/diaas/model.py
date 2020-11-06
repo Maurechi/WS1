1f355528a2d3199ff3ff4f63bd52eda6fce50e62
Marco Baringer <marco@leukos.io>
2020-11-06 22:10:55 +0100

    impr(be/schema): workbench, not session.

--- be/src/diaas/model.py
+++ be/src/diaas/model.py
@@ -12,9 +12,25 @@ def hashid_computed(column_name):
     )
 
 
-class Session(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
+class User(db.Model):
+    uid = db.Column(db.Integer(), primary_key=True)
     code = db.Column(db.String(), hashid_computed("id"))
+    email = db.Column(db.String(), unique=True)
+
+
+class Warehouse(db.Model):
+    whid = db.Column(db.Integer(), primary_key=True)
+    code = db.Column(db.String(), hashid_computed("id"))
+    repo = db.Column(db.String(), nullable=False)
+
+
+class Workbench(db.Model):
+    wbid = db.Column(db.Integer(), primary_key=True)
+    code = db.Column(db.String(), hashid_computed("id"))
+    uid = db.Column(db.Integer(), db.ForeignKey("user.uid"), nullable=False)
+    whid = db.Column(db.Integer(), db.ForeignKey("warehouse.whid"), nullable=False)
+    branch = db.Column(db.String(), default="master", nullable=False)
+
     first_created = db.Column(db.DateTime(), server_default=db.text("now()::timestamp"))
     last_modified = db.Column(
         db.DateTime(),
@@ -33,8 +49,3 @@ class Session(db.Model):
         if not local_dir.exists():
             self.create_local_data(local_dir)
         return local_dir
-
-
-class User(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    code = db.Column(db.String(), hashid_computed("id"))
