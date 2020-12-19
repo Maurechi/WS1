9d0d79433011c1f0f44b1343def34069a9cafa51
Marco Baringer <marco@leukos.io>
2020-12-19 11:17:17 +0100

    impr(fe): move the mock user data to the backend code so we at least exercise the api calling code

--- be/src/diaas/app/v1.py
+++ be/src/diaas/app/v1.py
@@ -25,6 +25,73 @@ def _user_as_json(user):
     }
 
 
+MOCK_USER = dict(
+    uid="q18y",
+    workbenches=[
+        dict(
+            wbid="kauw",
+            name="master",
+            branch="master",
+            warehouse=dict(
+                whid="a7rt",
+                name="Astrospace GmbH",
+            ),
+            branches=dict(
+                master=dict(
+                    files={
+                        "pw3B": dict(
+                            id="pw3B",
+                            name="rockets_d.sql",
+                            details="Locally Modified",
+                            lastModified="Today",
+                            code=(
+                                "select\n"
+                                "  timestamp(json_extract(data, '$.launchDate')) as launch_date\n"
+                                "  ,json_extract(data, '$.status')) as status\n"
+                                "  ,upper(json_extract(data, '$.launchPad'))) as launch_pad\n"
+                                "from rockets_r;"
+                            ),
+                        ),
+                        "orhj": dict(
+                            id="orhj",
+                            name="launches_d.sql",
+                            details="Unmodified",
+                            lastModified="1 Week Ago (Nov 3rd, 2020)",
+                        ),
+                        "mzsk": dict(
+                            id="mzsk",
+                            name="conversions_f.sql",
+                            details="Unmodified",
+                            lastModified="1 Week Ago (Nov 3rd, 2020)",
+                        ),
+                        "9x27": dict(
+                            id="9x27",
+                            name="rockets_cleaned.py",
+                            details="Unmodified",
+                            lastModified="1 Week Ago (Nov 3rd, 2020)",
+                            code=(
+                                "import pandas as pd\n"
+                                "from diaas_modules.geo import Geo, AddressNotFound \n"
+                                "\n"
+                                "def transform_row(row):\n"
+                                "    if row.lat is not None and row.lon is not None:\n"
+                                "        row.geo = Geo.locate(row.lat, row.lon)\n"
+                                "    elif row.address_text is not None:\n"
+                                "        try:\n"
+                                "            row.geo = Geo.geocode(row.address_text)\n"
+                                "        except AddressNotFound:\n"
+                                "            row.geo = None\n"
+                            ),
+                        ),
+                    },
+                    tree=["pw3B", "orhj", "mzsk", "9x27"],
+                ),
+            ),
+        ),
+    ],
+)
+
+
 @api_v1.route("/user", methods=["GET"])
 @as_json
 def session_get():
@@ -33,7 +100,7 @@ def session_get():
         if u is None:
             return {}, 404
         else:
-            return _user_as_json(u)
+            return MOCK_USER  # _user_as_json(u)
     else:
         return {}, 404
 
