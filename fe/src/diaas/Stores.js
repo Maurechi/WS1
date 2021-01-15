2db890c28bd11e6c8995e37b6153bb4979e771ef
Marco Baringer <marco@leukos.io>
2021-01-15 12:31:49 +0100

    impr(fe): slightly prettier sources and store pages

--- fe/src/diaas/Stores.js
+++ fe/src/diaas/Stores.js
@@ -9,14 +9,40 @@ export const StoresContent = () => {
     return <Box>No Stores.</Box>;
   } else {
     const { id, type, ...config } = stores[0];
+    const icon = type === "libds.store.postgresql.PostgreSQL" ? "postgresql.svg" : "404.gif";
+    const alt = type === "libds.store.postgresql.PostgreSQL" ? "PostgreSQL DB" : "Unknown Type";
 
     return (
       <>
         <p>
-          Store `{id}` is a {type}.
+          <img width="100em" src={`/i/logos/${icon}`} alt={alt} />
         </p>
-        <p>Configuration:</p>
-        <pre>{JSON.stringify(config, null, 4)}</pre>
+        <table>
+          <tr>
+            <td>Id:</td>
+            <td>
+              <tt>{id}</tt>
+            </td>
+          </tr>
+          <tr>
+            <td>Host:</td>
+            <td>{config.parameters.host}</td>
+          </tr>
+          <tr>
+            <td>Port:</td>
+            <td>{config.parameters.port}</td>
+          </tr>
+          <tr>
+            <td>User:</td>
+            <td>{config.parameters.user}</td>
+          </tr>
+          <tr>
+            <td>Parameters:</td>
+            <td>
+              <pre>{JSON.stringify(config, null, 4)}</pre>
+            </td>
+          </tr>
+        </table>
       </>
     );
   }
