e475415873130b6593916fa0742d72d5153f5740
Marco Baringer <marco@leukos.io>
2021-01-23 19:34:40 +0100

    impr(ops/secrets): allow slashes and @ char in secret names

--- ops/src/diaas_ops/secret.py
+++ ops/src/diaas_ops/secret.py
@@ -1,114 +1,164 @@
-import re
 import os
+import re
 import sys
 from pprint import pprint  # noqa: F401
 
 import click
 import google.auth
-from google.cloud import secretmanager
-from google.cloud import resource_manager
-
-
-class Secret:
-    def __init__(self, client, secret):
-        self.client = client
-        self.secret = secret
-        self.name = secret.name
-
-    def list_versions(self, page_token=None):
-        res = self.client.secret_manager.list_secret_versions(
-            request={"parent": self.name, "page_token": page_token}
-        )
-        for v in res.versions:
-            yield Version(secret=self, name=v.name)
-        if res.next_page_token:
-            yield from self.list_versions(res.next_page_token)
-
-    @property
-    def versions(self):
-        return self.list_versions()
-
-    @property
-    def display_name(self):
-        m = re.match(r'^projects/(?P<project_number>[^/]+)/secrets/(?P<secret_id>.*)$', self.name)
-        if m:
-            project_id = self.client.project_id(m.group("project_number"))
-            secret_id = m.group("secret_id")
-            return project_id + "/" + secret_id
-        else:
-            return self.name
-
-    def get_version(self, version):
-        return Version(secret=self, name=self.name + "/versions/" + version)
-
-    def add_version(self, value):
-        res = self.client.secret_manager.add_secret_version(
-            request={"parent": self.name, "payload": {"data": value.encode("utf-8")}}
-        )
-        return Version(secret=self, name=res.name)
-
-
-class Version:
-    def __init__(self, secret, name):
-        self.secret = secret
-        self.name = name
-
-    @property
-    def value(self):
-        try:
-            res = self.secret.client.secret_manager.access_secret_version(
-                request={"name": self.name}
-            )
-        except google.api_core.exceptions.NotFound:
-            return None
-        except google.api_core.exceptions.InvalidArgument:
-            return None
-        return res.payload.data
+from google.cloud import resource_manager, secretmanager
 
 
 class Client:
     def __init__(self):
-        self.credentials, self.project = google.auth.default()
-        self.secret_manager = secretmanager.SecretManagerServiceClient( credentials=self.credentials )
-        self.resource_manager = resource_manager.Client(credentials=self.credentials)
-
-    def list_secrets(self, page_token=None):
-        res = self.secret_manager.list_secrets(
-            request={"parent": f"projects/{self.project}", "page_token": page_token}
+        self.credentials, self.project_id = google.auth.default()
+        self.secret_manager = secretmanager.SecretManagerServiceClient(
+            credentials=self.credentials
         )
-        for s in res.secrets:
-            yield Secret(client=self, secret=s)
-        if res.next_page_token:
-            yield from self.list_secrets(self.project, res.next_page_token)
+        self.resource_manager = resource_manager.Client(credentials=self.credentials)
 
     @property
     def secrets(self):
-        return self.list_secrets()
-
-    def get_secret(self, secret_id):
-        try:
-            res = self.secret_manager.get_secret(
-                request={"name": f"projects/{self.project}/secrets/{secret_id}"}
+        def _list(page_token):
+            res = self.secret_manager.list_secrets(
+                request={
+                    "parent": f"projects/{self.project_id}",
+                    "page_token": page_token,
+                }
             )
+            for s in res.secrets:
+                yield Secret(client=self, resource=s.name)
+            if res.next_page_token:
+                yield from _list(res.next_page_token)
+
+        return sorted(_list(None), key=lambda s: s.resource)
+
+
+class SecretVersion:
+    def __init__(self, client, resource):
+        self.client = client
+        self.resource = resource
+
+    @property
+    def value(self):
+        try:
+            res = self.client.secret_manager.access_secret_version(
+                request={"name": self.resource}
+            )
+            return res.payload.data
         except google.api_core.exceptions.NotFound:
             return None
-        return Secret(self, res)
+        except google.api_core.exceptions.InvalidArgument:
+            return None
 
-    def create_secret(self, secret_id):
-        self.secret_manager.create_secret({
-            "parent": f"projects/{self.project}",
-            "secret_id": secret_id,
-            "secret": {"replication": {"automatic": {}}},
-        })
-        return self.get_secret(secret_id)
+    @property
+    def exists(self):
+        return self.value is not None
 
-    def project_id(self, project_number):
-        return self.resource_manager.fetch_project(project_number).name
 
-    def project_number(self, project_id):
-        prj = resource_manager.project.Project(project_id=project_id)
-        prj.reload()
-        return prj.project_number
+def quote_name(name):
+    def quote(m):
+        code = ord(m[0])
+        if code > 255:
+            raise ValueError("Can't encode {char} in with 2 hex digits.")
+        return "_{:02x}".format(code)
+
+    return re.sub("[^a-zA-Z0-9]", quote, name)
+
+
+def unquote_name(name):
+    def unquote(m):
+        return chr(int(m[1], base=16))
+
+    return re.sub("_([0-9a-fA-F]{2})", unquote, name)
+
+
+class Secret:
+    def __init__(self, client=None, resource=None):
+        if client is None:
+            client = Client()
+        self.client = client
+        self.resource = resource
+
+    @classmethod
+    def from_name(cls, name):
+        client = Client()
+        return cls(
+            client=client,
+            resource=f"projects/{client.project_id}/secrets/{quote_name(name)}",
+        )
+
+    def _ids(self):
+        m = re.match(
+            r"^projects/(?P<project_number>[^/]+)/secrets/(?P<secret_id>.*)$",
+            self.resource,
+        )
+        if m:
+            return (m.group("project_number"), m.group("secret_id"))
+        else:
+            return (None, None)
+
+    @property
+    def project_number(self):
+        return self._ids()[0]
+
+    @property
+    def project_id(self):
+        return self.client.resource_manager.fetch_project(self.project_number).name
+
+    @property
+    def secret_id(self):
+        return self._ids()[1]
+
+    @property
+    def name(self):
+        return unquote_name(self.secret_id)
+
+    @property
+    def display_name(self):
+        return self.project_id + "/" + self.name
+
+    @property
+    def exists(self):
+        try:
+            self.client.secret_manager.get_secret(request={"name": self.resource})
+            return True
+        except google.api_core.exceptions.NotFound:
+            return False
+        return True
+
+    def create(self, exists_ok=True):
+        if self.exists:
+            if exists_ok:
+                return self
+            else:
+                raise Exception(f"Secret {self.resource} already exists")
+        self.client.secret_manager.create_secret(
+            {
+                "parent": f"projects/{self.project_id}",
+                "secret_id": self.secret_id,
+                "secret": {"replication": {"automatic": {}}},
+            }
+        )
+        return self
+
+    def get_version(self, version):
+        return SecretVersion(
+            client=self.client, resource=f"{self.resource}/versions/{version}"
+        )
+
+    @property
+    def value(self):
+        return self.get_version("latest").value
+
+    @value.setter
+    def value(self, new_value):
+        self.client.secret_manager.add_secret_version(
+            request={
+                "parent": self.resource,
+                "payload": {"data": new_value.encode("utf-8")},
+            }
+        )
+        return self.value
 
 
 @click.group()
@@ -124,39 +174,38 @@ def list():
 
 
 @cli.command()
-@click.argument("secret_id")
+@click.argument("name")
 @click.option("-v", "--version", default="latest")
 @click.option("-r", "--raw", is_flag=True)
 @click.pass_context
-def get(ctx, secret_id, version, raw):
-    client = Client()
-    s = client.get_secret(secret_id)
-    if s is None:
-        click.echo(f"No secret named {secret_id}")
+def get(ctx, name, version, raw):
+    s = Secret.from_name(name)
+    if not s.exists:
+        click.echo(f"No secret named {name}")
         ctx.exit(2)
-    value = s.get_version(version).value
-    if value is None:
-        click.echo(f"No secret names {secret_id}:{version} found.")
+    v = s.get_version(version)
+    if not v.exists:
+        click.echo(f"No version {name}:{version} found.")
         ctx.exit(2)
     if raw:
         with os.fdopen(sys.stdout.fileno(), "wb", closefd=False) as out:
-            out.write(value)
+            out.write(v.value)
     else:
-        print(value.decode("utf-8"))
+        print(v.value.decode("utf-8"))
 
 
 @cli.command()
-@click.argument("secret_id")
+@click.argument("name")
 @click.argument("value")
 @click.pass_context
-def set(ctx, secret_id, value):
-    client = Client()
-    s = client.get_secret(secret_id)
-    if s is None:
-        s = client.create_secret(secret_id)
-    version = s.add_version(value)
-    print(version.name)
+def set(ctx, name, value):
+    s = Secret.from_name(name)
+    if not s.exists:
+        s.create()
+    if value == "-":
+        value = sys.stdin.read()
+    s.value = value
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
