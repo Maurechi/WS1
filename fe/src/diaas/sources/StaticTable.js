f254243d74d4d42233dee92aaa3da579ab3f8cbe
Marco Baringer <marco@leukos.io>
2021-05-21 18:08:31 +0200

    fix(fe/StaticTables): properly redirect to new url when changing source id

--- fe/src/diaas/sources/StaticTable.js
+++ fe/src/diaas/sources/StaticTable.js
@@ -2,6 +2,7 @@ import { Grid, Typography } from "@material-ui/core";
 import _ from "lodash";
 import { observer } from "mobx-react-lite";
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 import { SampleDataTable } from "diaas/DataTable.js";
 import { Form, TextField, useFormValue } from "diaas/form.js";
@@ -10,6 +11,35 @@ import { ActionButton } from "diaas/ui.js";
 
 export const iconURL = "csv.png";
 export const label = "Manual Data Entry";
+
+const useSourceFileUpdater = () => {
+  const { user, backend } = useAppState();
+  const history = useHistory();
+  return ({ src_id, dst_id, data }) => {
+    const src = `sources/${src_id}.yaml`;
+    const dst = `sources/${dst_id}.yaml`;
+
+    return backend
+      .postFile(src, data)
+      .then(() => {
+        if (src !== dst) {
+          return backend.moveFile(src, dst);
+        } else {
+          return Promise.resolve(null);
+        }
+      })
+      .then(() => {
+        return backend.sourceInfo(dst_id);
+      })
+      .then((source) => {
+        let sources = user.data_stacks[0].sources;
+        sources = _.filter(sources, (s) => s.id !== src_id);
+        sources.push(source);
+        user.data_stacks[0].sources = sources;
+        return history.replace(`/sources/${dst_id}`);
+      });
+  };
+};
 
 export const Editor = observer(({ source }) => {
   if (!source) {
@@ -29,25 +59,21 @@ export const Editor = observer(({ source }) => {
   const table = useFormValue(source.data.table);
   const data = useFormValue(source.data.data, { trim: false });
   const rows = source.info.rows.map((row) => Object.fromEntries(_.zip(source.info.columns, row)));
-  const { backend } = useAppState();
+
+  const sourceFileUpdater = useSourceFileUpdater();
+
   const save = () => {
-    let src, dst;
-    if (source.filename === null) {
-      src = `sources/${table.v}.yaml`;
-      dst = src;
-    } else {
-      src = `sources/${source.filename}`;
-      dst = `sources/${table.v}.yaml`;
-    }
-    return backend
-      .postFile(src, {
+    const src_id = source.filename === null ? table.v : source.data.table;
+    const dst_id = table.v;
+    return sourceFileUpdater({
+      src_id,
+      dst_id,
+      data: {
         data: data.v,
         table: table.v,
         type: source.data.type,
-      })
-      .then(() => {
-        return src !== dst ? backend.moveFile(src, dst) : null;
-      });
+      },
+    });
   };
   return (
     <Form onSubmit={save}>
